d4ec1765a171c6898320b6b4b8297af3ed6dd7b9
ayanda <ayandamantshinga30@gmail.com>
2025-09-13 21:21:03 +0200

    Initial commit

---
+++ app/src/main/java/com/xdevstudio/realtimedb/RegistrationsActivity.kt
@@ -0,0 +1,54 @@
+package com.xdevstudio.realtimedb
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+
+class RegistrationsActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registrations)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
+        { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        auth = FirebaseAuth.getInstance()
+
+        val emailInput = findViewById<EditText>(R.id.emailInput)
+        val passwordInput = findViewById<EditText>(R.id.passwordInput)
+        val registerButton = findViewById<Button>(R.id.registerButton)
+
+        registerButton.setOnClickListener {
+            val email = emailInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            auth.createUserWithEmailAndPassword(email , password)
+                .addOnCompleteListener(this)
+                { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Registered successfully!", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, LoginActivity::class.java))
+                        finish()
+                    }else{
+                        Toast.makeText(this, "Failed: ${task.exception?.message}",Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+}
